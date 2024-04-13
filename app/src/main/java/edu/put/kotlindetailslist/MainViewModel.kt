6a45e4a59d17f456dd21563752c09af26b4c149c
Maciej Mazur <maciek.m2317@vp.pl>
2024-04-13 11:11:32 +0200

    Version II

--- app/src/main/java/edu/put/kotlindetailslist/MainViewModel.kt
+++ app/src/main/java/edu/put/kotlindetailslist/MainViewModel.kt
@@ -4,22 +4,25 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import edu.put.kotlindetailslist.database.Trail
 
-interface Server{
-    fun loadDataFromDB(context: Context?):List<Trail>
+interface Server {
+    fun loadDataFromDB(context: Context?, difficulty: String): List<Trail>
 }
 
 class MainViewModel : ViewModel(), Server {
     private var trail: Trail? = null
     private var service = TrailService()
 
-    override fun loadDataFromDB(context: Context?): List<Trail> {
-        return service.findAllTrails(context);
+    override fun loadDataFromDB(context: Context?, difficulty: String): List<Trail> {
+        return service.findAllTrails(context)
+            .filter {
+                it.difficulty == difficulty
+            }
     }
 
-    fun setTrail(trail:Trail){
+    fun setTrail(trail: Trail) {
         this.trail = trail
     }
 
-    fun getTrail()=trail
+    fun getTrail() = trail
 
 }
--- app/src/main/java/edu/put/kotlindetailslist/StartFragment.kt
+++ app/src/main/java/edu/put/kotlindetailslist/StartFragment.kt
@@ -1,12 +1,12 @@
 package edu.put.kotlindetailslist
 
 import android.os.Bundle
+import android.view.*
+import androidx.activity.addCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import edu.put.kotlindetailslist.databinding.ActivityMainBinding
 import edu.put.kotlindetailslist.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
@@ -17,16 +17,15 @@ class StartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding =  FragmentStartBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.startText.setOnClickListener{
-            findNavController().navigate(R.id.action_startFragment_to_listFragment)
-        }
+//        binding.startText.setOnClickListener {
+//            findNavController().navigate(R.id.action_startFragment_to_listFragment)
+//        }
     }
 
     override fun onDestroyView() {
